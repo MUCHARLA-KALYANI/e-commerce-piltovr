66f18d47bfda4a5f5952e99e79822fc01c485894
KALYANI <22b01a12a0@svecw.edu.in>
2024-05-28 19:08:09 +0530

    color.tsx and size.tsx for product form are added

---
+++ app/components/Size.tsx
@@ -0,0 +1,45 @@
+import React,{useState} from "react"
+
+interface ParaProps {
+    setFormData:React.Dispatch<React.SetStateAction<any>>
+}
+const Size : React.FC<ParaProps> = ({setFormData}) => {
+    const [selectedSizes,setSelectedSizes]=useState<string[]>([])
+    const sizes =["sm","md","xl","2xl","3xl","4xl"]
+
+    const handleSizeButtonClick=(size: string) =>{
+        setSelectedSizes((prevSelectedSizes)=>{
+            if(prevSelectedSizes.includes(size)){
+                return prevSelectedSizes.filter((s)=>s !== size)
+            }
+            else{
+                return[...prevSelectedSizes,size]
+            }
+        })
+    }
+
+    const handleSubmit = () =>{
+        setFormData((prevFormData: FormData)=>({
+          ...prevFormData ,
+          size:selectedSizes.join(',')
+        }))
+    }
+
+
+   return(
+    <div>
+        {sizes.map((size)=>(
+            <button
+            key={size}
+            className={`border-[0.5px] rounded-lg text-center text-[14px] py-[2px] cursor-pointer px-3 mt-4 mb-5 mr-5 
+            ${selectedSizes.includes(size) ? "bg-gray-500":""}`}
+            onClick={()=> handleSizeButtonClick(size)}>
+            {size}
+            </button>
+        ))}
+        <button onClick={handleSubmit}>Submit</button>
+    </div>
+   )
+}
+
+export default Size
